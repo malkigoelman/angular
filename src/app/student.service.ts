ea7a3674b7762ac3c5024d3990b8c40a8c2c1298
malki <malki79292@gmail.com>
2024-02-04 00:38:59 +0200

    😶

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Student } from "./models/student.modle";
 import { Observable } from "rxjs";
-
-const STUDENTS: Student[] = []
+import {HttpClient} from "@angular/common/http"
+//const STUDENTS: Student[] = []
     //     return [
     //         {id:1,firstName:"malki",lastname:"goelman",address:"aa 12",phone: 123,activ:true,AvgMark:90},
     //         {id:1,firstName:"sari",lastname:"goelman",address:"dfy g",phone: 456,activ:true},
     //         {id:1,firstName:"batya",lastname:"goelman",address:"nh 23",phone: 789,activ:false,AvgMark:95}        
     //     ]
-    ;
+ //   ;
 @Injectable()
 export class StudentService {
 
@@ -20,24 +20,31 @@ export class StudentService {
     //     })
     // }
     getStudentForServer(): Observable<Student[]>{
-          return this._http.get<Student[]>("https://localhost:7028/api/student");
+          return this._http.get<Student[]>("/api/student");
     }
     getStudentFromServerByISActiv(isActive:boolean):Observable<Student[]>{
         return this._http.get<Student[]>("/api/student/?isActiv="+isActive)
     }
+    getStudentFromServerByName(name:string):Observable<Student[]>{
+        return this._http.get<Student[]>("/api/student/?name="+name)
+    }
     seveStudents(studentList:Student[]):Observable<boolean>{
-        return this._http.post<Boolean>("/api/student",studentList);
+        return this._http.post<boolean>("/api/student",studentList);
     }
-
-
-    getStudent(): Student[] {
-        return STUDENTS;
+    updateStudent(student:Student):Observable<boolean>{
+        return this._http.put<boolean>("api/student/${student.ud}",student);
     }
-    getStudentSlowly(): Promise<Student[]> {
-        return new Promise((res, rej) => {
-            setTimeout(() => { res(STUDENTS) }, 5000);
-        })
+    deleteStudent(id:number):Observable<boolean>{
+        return this._http.delete<boolean>("api/student/"+id);
     }
+    // getStudent(): Student[] {
+    //     return STUDENTS;
+    // }
+    // getStudentSlowly(): Promise<Student[]> {
+    //     return new Promise((res, rej) => {
+    //         setTimeout(() => { res(STUDENTS) }, 5000);
+    //     })
+    // }
     getAverageById(id: number) {
         return STUDENTS.filter(x => x.id == id)[0].avgMarks;
     }
